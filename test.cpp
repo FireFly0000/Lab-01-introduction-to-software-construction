b3b018ccfbfdff8a4874ad6ad139de6cb6c4a439
FireFly0000 <wktfirefly283@gmail.com>
2021-03-31 14:44:48 -0700

    Add 4 more unit googletest cases for echo function

--- test.cpp
+++ test.cpp
@@ -7,6 +7,28 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, SingleSpace) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = " "; 
+    EXPECT_EQ(" ", echo(2,test_val));
+}
+
+TEST(EchoTest, SpaceBeforeChars) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = " TEST2";
+    EXPECT_EQ(" TEST2", echo(2,test_val));
+}
+
+TEST(EchoTest, SpaceAfterChars) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "TEST3 ";
+    EXPECT_EQ("TEST3 ", echo(2,test_val));
+}
+
+
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
